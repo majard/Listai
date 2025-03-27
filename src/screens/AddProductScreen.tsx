2cc348a2aeea012f02a75d935a075f17dbe67f21
majard <mahjard@protonmail.com>
2025-03-27 19:55:36 -0300

    Added list to text button

--- src/screens/AddProductScreen.tsx
+++ src/screens/AddProductScreen.tsx
@@ -11,6 +11,7 @@ type AddProductScreenNavigationProp = NativeStackNavigationProp<RootStackParamLi
 export default function AddProductScreen() {
   const [name, setName] = useState('');
   const [quantity, setQuantity] = useState('0');
+  const [weight, setWeight] = useState('0');
   const navigation = useNavigation<AddProductScreenNavigationProp>();
   const theme = useTheme();
 
@@ -18,7 +19,8 @@ export default function AddProductScreen() {
     try {
       await addProduct(
         name,
-        parseInt(quantity, 10)
+        parseInt(quantity, 10),
+        parseInt(weight, 10)
       );
       navigation.goBack();
     } catch (error) {
@@ -43,11 +45,19 @@ export default function AddProductScreen() {
         style={styles.input}
         mode="outlined"
       />
+      <PaperTextInput
+        label="Peso (g)"
+        value={weight}
+        onChangeText={setWeight}
+        keyboardType="numeric"
+        style={styles.input}
+        mode="outlined"
+      />
       <Button
         mode="contained"
         onPress={handleSubmit}
         style={styles.button}
-        disabled={!name || !quantity}
+        disabled={!name || !quantity || !weight}
       >
         Adicionar Produto
       </Button>
