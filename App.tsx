409129679a04b825d75a98c66e5fbe6e7336678a
majard <mahjard@protonmail.com>
2025-06-01 19:27:55 -0300

    Fix addlist page title

--- App.tsx
+++ App.tsx
@@ -69,6 +69,7 @@ export default function App() {
               />
               <Stack.Screen
               name="AddList"
+              options={{title: 'Adicionar Lista'}}
               component={AddListScreen}
               />
             </Stack.Navigator>
